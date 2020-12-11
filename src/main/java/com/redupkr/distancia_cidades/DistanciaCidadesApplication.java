804e4481f650e9bb0051c2a571d20d7a55913938
redu <reduardo19942@gmail.com>
2020-12-11 15:58:33 -0300

    Projeto iniciado

---
+++ src/main/java/com/redupkr/distancia_cidades/DistanciaCidadesApplication.java
@@ -0,0 +1,13 @@
+package com.redupkr.distancia_cidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanciaCidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanciaCidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redupkr/distancia_cidades/DistanciaCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redupkr.distancia_cidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanciaCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
