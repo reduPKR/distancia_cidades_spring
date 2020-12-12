acdc79958c035049a078be5a3c9a768e12c991ab
redu <reduardo19942@gmail.com>
2020-12-11 23:43:21 -0300

    get country by id and pagination

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Sistema para calcular a distancia entre duas cidades
 Postgres  
   
 ## Docker  
-docker run --name cities-db -d -p 5432:5432 -e POSTGRES_USER=postgres_user_city -e POSTGRES_PASSWORD=super_password -e POSTGRES_DB=cities postgres
+docker run --name cities-db -d -p 5432:5432 -e POSTGRES_USER=postgres_user_city -e POSTGRES_PASSWORD=super_password -e POSTGRES_DB=cities postgres  
+  
+## Rotas  
+http://localhost:8080/countries/all
+http://localhost:8080/countries/page?page={int}
--- src/main/java/com/redupkr/distancia_cidades/CountryResource.java
+++ src/main/java/com/redupkr/distancia_cidades/CountryResource.java
@@ -3,14 +3,18 @@ package com.redupkr.distancia_cidades;
 import com.redupkr.distancia_cidades.entity.Country;
 import com.redupkr.distancia_cidades.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/counrty")
+@RequestMapping("/countries")
 public class CountryResource {
     @Autowired
     private CountryRepository repository;
@@ -18,6 +22,17 @@ public class CountryResource {
     @GetMapping("/all")
     public List<Country> findAll(){
         return repository.findAll();
+    }
+
+    @GetMapping("/page")
+    public Page<Country> getPage(Pageable page){
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public Country getById(@PathVariable long id){
+        Optional<Country> optional = repository.findById(id);
+        return optional.get();
     }
 
 }
